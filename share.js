aefea9ad36b81d49e5dcf63ae796ef245569c389
modernWoman <74977806+modernWoman@users.noreply.github.com>
2020-12-04 21:44:57 +0900

    Add files via upload

---
+++ share.js
@@ -0,0 +1,40 @@
+<script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
+
+<a href="javascript:;" id="kakao-link-btn">
+<img src="//developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_small.png" width="10px" />
+</a>
+
+<script type="text/javascript">
+  //<![CDATA[
+    // // 사용할 앱의 JavaScript 키를 설정해 주세요.
+    Kakao.init('bfd4cd7749e2b1ffca24a00eeb5aaabc');
+    // // 카카오링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
+    Kakao.Link.createDefaultButton({
+      container: '#kakao-link-btn',
+      objectType: 'feed',
+      content: {
+        title: document.title,
+        description: '내용, 주로 해시태그',
+        imageUrl: document.images[0].src,
+        link: {
+          webUrl: document.location.href,
+          mobileWebUrl: document.location.href
+        }
+      },
+      social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845
+      },
+      buttons: [
+        {
+          title: 'Open!',
+          link: {
+            mobileWebUrl: document.location.href,
+            webUrl: document.location.href
+          }
+        }  
+      ]
+    });
+  //]]>
+</script>
